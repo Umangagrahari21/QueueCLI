4f7c27c606da721c5a35781156204667ca5589cf
Umang Agrahari <agrahariumang222005@gmail.com>
2026-07-17 00:05:19 +0530

    chore: initialize QueueCTL project foundation

---
+++ src/cli/index.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+import { Command } from "commander";
+import db from "../database/db.js";
+
+const program = new Command();
+
+program
+  .name("queuectl")
+  .description("CLI-based Background Job Queue")
+  .version("1.0.0");
+
+program
+  .command("hello")
+  .description("Test CLI")
+  .action(() => {
+    console.log("QueueCTL is working ✅");
+  });
+
+program.parse(process.argv);
